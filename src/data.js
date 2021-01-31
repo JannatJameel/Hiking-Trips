206488503a58b08577619e33eee73967fb425f3e
JannatJameel <j_annat@outlook.com>
2021-01-31 10:01:06 +0300

    data done

---
+++ src/data.js
@@ -0,0 +1,65 @@
+const data = [
+    {
+        id : 1,
+        name: "South West Coast Path",
+        slug: "south-west-coast-path",
+        image: "https://www.muchbetteradventures.com/magazine/content/images/2021/01/South-west-coast.jpg",
+        city: "Cornwall",
+        difficulty: "Medium",
+        length: "117", //km
+        rating: 4,
+    },
+    {
+        id : 2,
+        name: "The Quiraing",
+        slug: "the-quiraing",
+        image: "https://www.muchbetteradventures.com/magazine/content/images/2021/01/Isle-of-skye.jpg",
+        city: "Isle of Skye",
+        difficulty: "Medium",
+        length: "80",
+        rating: 3,
+    },
+    {
+        id : 3,
+        name: "South Downs Way",
+        slug: "south-downs-way",
+        image: "https://www.muchbetteradventures.com/magazine/content/images/2021/01/South-downs.jpg",
+        city: "Hampshire",
+        difficulty: "Hard",
+        length: "240",
+        rating: 5,
+    },
+    {
+        id : 4,
+        name: "Scafell Pike",
+        slug: "",
+        image: "https://www.muchbetteradventures.com/magazine/content/images/2021/01/Scaffell.jpg",
+        city: "Lake District",
+        difficulty: "Easy",
+        length: "0.98",
+        rating: 4,
+    },
+    {
+        id : 5,
+        name: "Coast to Coast",
+        slug: "coast-to-coast",
+        image: "https://www.muchbetteradventures.com/magazine/content/images/2021/01/yoirkshire.jpg",
+        city: "North Yorkshire",
+        difficulty: "Hard",
+        length: "320",
+        rating: 4,
+    },
+    {
+        id : 6,
+        name: "Yorkshire Three Peaks",
+        slug: "yorkshire-three-peaks",
+        image: "https://www.muchbetteradventures.com/magazine/content/images/2021/01/Yorkshire-Dales.jpg",
+        city: "Yorkshire",
+        difficulty: "Easy",
+        length: "1.6", //Km
+        rating: 5, //
+    },
+    
+];
+
+export default data; 
